d682ef24c9bea2d22f11b02be44f923a5bdc01e4
KamilKurde <kamilbak113@gmail.com>
2024-09-25 21:06:49 +0200

    feat: add basic logs

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,40 +1,37 @@
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
-import dev.kord.core.entity.application.ChatInputCommandCommand
-import dev.kord.core.event.message.MessageCreateEvent
-import dev.kord.core.on
 import dev.kord.gateway.*
 import dev.kord.rest.builder.interaction.ChatInputCreateBuilder
 import dev.kord.rest.builder.interaction.string
+import kotlinx.coroutines.runBlocking
+import org.slf4j.MarkerFactory
 
 class Bot {
-	private var commands = emptyList<ChatInputCommandCommand>()
-
 	@OptIn(PrivilegedIntent::class)
 	suspend fun initialize(kord: Kord) {
+		val tag = MarkerFactory.getMarker("Bot#initialize")
+		logger.info(tag, "Removing old commands")
 		kord.removeOldCommands()
+		logger.info(tag, "Setting up new commands commands")
 		kord.setUpCommands()
-		kord.on<MessageCreateEvent>(consumer = ::onMessageCreate)
+		logger.info(tag, "Logging in")
 		kord.login {
 			intents = Intents(Intent.MessageContent, Intent.GuildMessages, Intent.GuildMessageReactions)
 		}
 	}
 
-	suspend fun cleanUp(kord: Kord) {
-		commands.forEach { command -> command.delete() }
+	fun cleanUp(kord: Kord) = runBlocking {
+		val tag = MarkerFactory.getMarker("Bot#cleanUp")
+		logger.info(tag, "Logging out")
 		kord.logout()
 	}
 
-	private suspend fun onMessageCreate(event: MessageCreateEvent) {
-
-	}
-
 	private suspend fun Kord.removeOldCommands() {
-		getGlobalApplicationCommands().collect{ command ->
+		getGlobalApplicationCommands().collect { command ->
 			command.delete()
 		}
-		guilds.collect{ guild ->
-			guild.getApplicationCommands().collect{ command ->
+		guilds.collect { guild ->
+			guild.getApplicationCommands().collect { command ->
 				command.delete()
 			}
 		}
@@ -50,12 +47,14 @@ class Bot {
 		}
 	}
 
-	private suspend fun Kord.registerCommand(name: String, description: String, builder: ChatInputCreateBuilder.() ->
-	Unit) {
-		val registeredCommand = when(val guildId = System.getenv("AresDiscordGuild").takeUnless { it.isNullOrBlank() }){
+	private suspend fun Kord.registerCommand(
+		name: String,
+		description: String,
+		builder: ChatInputCreateBuilder.() -> Unit
+	) {
+		when (val guildId = System.getenv("AresDiscordGuild").takeUnless { it.isNullOrBlank() }) {
 			null -> createGlobalChatInputCommand(name, description, builder)
 			else -> createGuildChatInputCommand(Snowflake(guildId), name, description, builder)
 		}
-		commands += registeredCommand
 	}
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,25 @@
 import dev.kord.core.Kord
-import org.slf4j.LoggerFactory
+import org.slf4j.*
 
-val logger = LoggerFactory.getLogger("Ares")
+val logger: Logger = LoggerFactory.getLogger("Ares")
 
 suspend fun main(){
-	val discordToken = System.getenv("AresDiscordToken").takeUnless { it.isNullOrBlank() } ?: return logger.error("Missing AresDiscordToken!")
+	val tag = MarkerFactory.getMarker("MainKt")
+	val discordToken = System.getenv("AresDiscordToken").takeUnless { it.isNullOrBlank() } ?: return logger.error(
+		tag,
+		"Missing AresDiscordToken!"
+	)
 
+	logger.info(tag, "Token acquired, creating Kord instance")
 	val kord = Kord(discordToken)
 
+	logger.info(tag, "Kord instance acquired, creating Bot instance")
 	val botInstance = Bot()
 
 	try {
 		botInstance.initialize(kord)
 	} finally {
+		logger.info(tag, "Cleaning up")
 		botInstance.cleanUp(kord)
 	}
 }
