9b209763a5c45c87396ac50d2e2757aca5555927
KamilKurde <kamilbak113@gmail.com>
2024-09-25 20:03:03 +0200

    chore: prepare gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+	alias(libs.plugins.kotlin)
+	alias(libs.plugins.shadow)
+	alias(libs.plugins.serialization)
+	application
+}
+
+group = "com.github.KamilKurde"
+version = "0.1.0"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(libs.kord.core)
+	implementation(libs.slf4j.simple)
+}
+
+application {
+	mainClass.set("MainKt")
+}
+
+tasks.withType(ShadowJar::class.java) {
+	archiveClassifier.set("")
+}
