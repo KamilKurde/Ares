3b7100c21ea44c6454f882635cbb18f48ab5f1a5
KamilKurde <kamilbak113@gmail.com>
2024-09-26 17:54:48 +0200

    feat: add status command

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,30 +1,30 @@
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
 import dev.kord.core.entity.application.ApplicationCommand
-import dev.kord.gateway.*
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.kord.core.on
 import dev.kord.rest.builder.interaction.ChatInputCreateBuilder
 import dev.kord.rest.builder.interaction.subCommand
 import kotlinx.coroutines.runBlocking
 import org.slf4j.MarkerFactory
 
 class Bot(private val guildId: Snowflake) {
-	@OptIn(PrivilegedIntent::class)
 	suspend fun initialize(kord: Kord) {
 		val tag = MarkerFactory.getMarker("Bot#initialize")
 		logger.info(tag, "Removing old commands")
 		kord.removeOldCommands()
+		logger.info(tag, "Setting up command hook")
+		kord.on<ChatInputCommandInteractionCreateEvent>(consumer = ::onCommand)
 		logger.info(tag, "Setting up new commands commands")
 		kord.setUpCommands()
 		logger.info(tag, "Logging in")
-		kord.login {
-			intents = Intents(Intent.MessageContent, Intent.GuildMessages, Intent.GuildMessageReactions)
-		}
+		kord.login()
 	}
 
 	fun cleanUp(kord: Kord) = runBlocking {
 		val tag = MarkerFactory.getMarker("Bot#cleanUp")
-		logger.info(tag, "Logging out")
-		kord.logout()
+		logger.info(tag, "Removing commands")
+		kord.removeOldCommands()
 	}
 
 	private suspend fun Kord.removeOldCommands() {
@@ -32,10 +32,16 @@ class Bot(private val guildId: Snowflake) {
 		getGuildApplicationCommands(guildId).collect(ApplicationCommand::delete)
 	}
 
+	private suspend fun onCommand(command: ChatInputCommandInteractionCreateEvent) {
+	}
+
 	private suspend fun Kord.setUpCommands() {
 		registerCommand("combat", "Controls status of a combat") {
 			subCommand("start", "Starts a combat")
 			subCommand("end", "Starts a combat")
+		}
+		registerCommand("status", "Shows status of a combat") {
+
 		}
 	}
 
