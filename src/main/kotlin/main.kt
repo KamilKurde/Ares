1551d0442e60d5f72f6f16d461bf20fea78d31f2
KamilKurde <kamilbak113@gmail.com>
2024-09-26 17:14:01 +0200

    fix: cleanUp not happening

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,11 @@
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
+import kotlinx.coroutines.runBlocking
 import org.slf4j.*
 
 val logger: Logger = LoggerFactory.getLogger("Ares")
 
-suspend fun main(){
+suspend fun main() {
 	val tag = MarkerFactory.getMarker("MainKt")
 	val discordToken = System.getenv("AresDiscordToken").takeUnless { it.isNullOrBlank() } ?: return logger.error(
 		tag,
@@ -21,10 +22,12 @@ suspend fun main(){
 	logger.info(tag, "Kord instance acquired, creating Bot instance")
 	val botInstance = Bot(Snowflake(guildId))
 
-	try {
-		botInstance.initialize(kord)
-	} finally {
-		logger.info(tag, "Cleaning up")
-		botInstance.cleanUp(kord)
-	}
+	Runtime.getRuntime().addShutdownHook(Thread {
+		runBlocking {
+			logger.info(tag, "Cleaning up")
+			botInstance.cleanUp(kord)
+		}
+	})
+
+	botInstance.initialize(kord)
 }
