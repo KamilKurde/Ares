b305556770e8c98834da258192bd4bb1f6d06c38
KamilKurde <kamilbak113@gmail.com>
2024-09-28 12:08:25 +0200

    chore: add KDoc for settings

--- src/main/kotlin/dev/kamilbak/ares/model/Settings.kt
+++ src/main/kotlin/dev/kamilbak/ares/model/Settings.kt
@@ -6,6 +6,12 @@ import net.peanuuutz.tomlkt.*
 import org.slf4j.MarkerFactory
 import java.io.File
 
+/**
+ * @property maxHp Maximum amount of HP a target can have
+ * @property bossHpLevel Amount of HP a target should have to be considered a "boss" and be coloured differently
+ * @property embedIcon Icon to use before combat name in status
+ * @property emojis Emojis to use in text messages
+ */
 @Serializable
 data class Settings(
 	val maxHp: Int = 100,
@@ -14,6 +20,11 @@ data class Settings(
 	val emojis: Emojis = Emojis()
 )
 
+/**
+ * @property combatStart used in combat start response
+ * @property attack used in response to attack command that delt damage but did not kill anyone
+ * @property kill used in response to attack command that killed a target
+ */
 @Serializable
 data class Emojis(
 	val combatStart: String? = null,
