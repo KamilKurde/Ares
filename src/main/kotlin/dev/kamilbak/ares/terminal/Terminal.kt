f05d7877475f010a01e697bdc1d192f4ee5f4771
KamilKurde <kamilbak113@gmail.com>
2024-09-29 20:52:33 +0200

    feat: add Terminal

---
+++ src/main/kotlin/dev/kamilbak/ares/terminal/Terminal.kt
@@ -0,0 +1,64 @@
+package dev.kamilbak.ares.terminal
+
+import dev.kord.common.entity.Snowflake
+
+data class Terminal(
+	val hacker: Snowflake,
+	val attemptsRemaining: Int,
+	val question: String,
+	val answers: List<Pair<String, AnswerType>>
+) {
+	companion object {
+		fun fromCommand(
+			hacker: Snowflake,
+			difficulty: Int,
+			unknowns: Int,
+			viruses: Int,
+			attemptsRemaining: Int,
+		): Terminal {
+			val correctAnswer: String
+
+			@Suppress("RemoveExplicitTypeArguments")
+			val answers = buildList<Pair<String, AnswerType>> {
+				fun generateCode(difficulty: Int): String {
+					val generated = buildString {
+						repeat(difficulty) {
+							append((0..15).random().toString(16))
+						}
+					}
+
+					val isUnique = none { (existing, _) -> existing == generated }
+
+					return if (isUnique) generated else generateCode(difficulty)
+				}
+
+				correctAnswer = generateCode(difficulty)
+				add(correctAnswer to AnswerType.Correct)
+				repeat(viruses) {
+					add(generateCode(difficulty) to AnswerType.Virus)
+				}
+				repeat(10 - size) {
+					add(generateCode(difficulty) to AnswerType.Incorrect)
+				}
+			}.shuffled()
+
+			return Terminal(hacker, attemptsRemaining, correctAnswer.obfuscate(unknowns), answers)
+		}
+
+		private fun String.obfuscate(chars: Int): String {
+			val indicesToReplace = (0..lastIndex).shuffled().take(chars)
+
+			return toCharArray().apply {
+				indicesToReplace.forEach { index ->
+					this[index] = '?'
+				}
+			}.concatToString()
+		}
+	}
+
+	enum class AnswerType {
+		Correct,
+		Incorrect,
+		Virus,
+	}
+}
---
+++ src/main/kotlin/dev/kamilbak/ares/terminal/Terminals.kt
@@ -0,0 +1,50 @@
+package dev.kamilbak.ares.terminal
+
+import dev.kamilbak.ares.logger
+import dev.kamilbak.ares.util.respondError
+import dev.kord.core.behavior.interaction.respondPublic
+import dev.kord.core.entity.interaction.ChatInputCommandInteraction
+import dev.kord.rest.builder.message.embed
+import org.slf4j.MarkerFactory
+
+class Terminals {
+	private val tag = MarkerFactory.getMarker("Terminals")
+	private val terminals: MutableMap<String, Terminal> = mutableMapOf()
+
+	private var onTerminalsChange: suspend () -> Unit = {}
+
+	suspend fun create(interaction: ChatInputCommandInteraction) {
+		val tag = MarkerFactory.getMarker("Terminals#create")
+		val name = interaction.command.strings["name"] ?: return interaction.respondError(tag, "name not found")
+		if (name in terminals.keys) return interaction.respondError(tag, "name already exists")
+
+		val hacker = interaction.command.users["hacker"] ?: return interaction.respondError(tag, "hacker not found")
+
+		val difficulty = interaction.command.integers["difficulty"]?.toInt() ?: 8
+		val unknowns = interaction.command.integers["unknowns"]?.toInt() ?: 4
+		val viruses = interaction.command.integers["viruses"]?.toInt() ?: 1
+
+		logger.info(
+			tag,
+			"Creating terminal for ${hacker.username}, with difficulty $difficulty, unknown $unknowns and viruses $viruses"
+		)
+		val terminal = Terminal.fromCommand(hacker.id, difficulty, unknowns, viruses, 2)
+		terminals[name] = terminal
+
+		interaction.respondPublic {
+			embed {
+				title = "CODE BREACH\u200F$name"
+				description = "SEQUENCER_${terminal.question}"
+			}
+		}
+
+		onTerminalsChange()
+	}
+
+	suspend fun registerOnTerminalsChange(register: suspend (targetNames: List<String>) -> Unit) {
+		onTerminalsChange = {
+			register(terminals.keys.sorted())
+		}
+		onTerminalsChange()
+	}
+}
